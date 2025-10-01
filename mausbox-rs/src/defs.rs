36a2df857177c97bd4b3fd9a2c51cb32b400252d
Giuseppe <djtech.developer@gmail.com>
2025-10-01 22:05:09 +0200

    Working on simulation

--- mausbox-rs/src/defs.rs
+++ mausbox-rs/src/defs.rs
@@ -5,22 +5,19 @@ pub struct RatBehaviour {
     // -- Reproduce Logic --
     reproduct_age_check: bool,
     reproduct_child_check: bool,
-    reproduct_delta_check: ReproductDeltaCheck,
-    reproduct_logic_gate: ReproductLogicCheck,
-
     reproduct_minimum_age: u16,
     reproduct_delta_limit1: f32,
     reproduct_delta_limit2: f32,
     reproduct_minimum_child: u16,
 
     minimum_food_to_reproduct: u16, // Only reproduct if food>limit
-    gift_food_to_child: u16, // Give X food to the child when he is born
+    gift_food_to_child: u16,        // Give X food to the child when he is born
 
     // -- Kill Logic --
     dont_kill_compatible: bool, // Don't kill rats that are compatible with you
-    kill_richest: bool, // Kill rat with most food IF you don't have any food
-    kill_oldest: bool,  // Kill oldest rat IF you don't have any food
-    kill_thief: bool,   // Kill rat that stole from you
+    kill_richest: bool,         // Kill rat with most food IF you don't have any food
+    kill_oldest: bool,          // Kill oldest rat IF you don't have any food
+    kill_thief: bool,           // Kill rat that stole from you
     revenge_parent: bool,
     revenge_brothers: bool,
     revenge_sons: bool,
@@ -28,13 +25,13 @@ pub struct RatBehaviour {
 
     // -- Steal Logic --
     dont_steal_compatible: bool, // Don't steal from rats that are compatible with you
-    dont_steal_family: bool, // Don't steal from parents, brothers or sons
-    steal_food_limit: u32, // If food<limit, try to steal food
-    steal_from_oldest: bool, // Prefer stealing from the eldest
-    steal_from_richest: bool, // Prefer stealing from the richest
+    dont_steal_family: bool,     // Don't steal from parents, brothers or sons
+    steal_food_limit: u32,       // If food<limit, try to steal food
+    steal_from_oldest: bool,     // Prefer stealing from the eldest
+    steal_from_richest: bool,    // Prefer stealing from the richest
 
     // -- Help Request Logic --
-    prefer_ask_compatible: bool, // Prefer asking help to compatible rats
+    prefer_ask_compatible: bool,  // Prefer asking help to compatible rats
     help_request_self_max: u32,   // Ask for help ONLY if self.food<self_max
     help_request_others_min: u32, // Ask for help ONLY if other.food>other_min
     // -- Help Response Logic --
@@ -51,7 +48,6 @@ pub enum Condition {
     others_age_major_than(u16),
     others_number_sons_major_than(u16),
     others_difference_minor_than(u16),
-
 }
 
 pub struct Environment {
